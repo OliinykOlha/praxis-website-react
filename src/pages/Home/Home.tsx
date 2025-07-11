d119acdd26a03ec3b4c7dc5a4a28ab445c28dbfe
OlhaOliinyk <olaolijnik71@gmail.com>
2025-07-11 10:19:54 +0200

    add new container to Homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,35 @@
-import { HomePicture, HomeDescription, HomeWrapper, Title } from "./styles";
-import Picture_1 from "../../assets/fizio.jpg";
+import { HomePicture, HomeDescription, HomeWrapper, Title, TwoColumnSection } from "./styles";
+import picture_1  from "../../assets/fizio.jpg";
+import picture_2 from "../../assets/fizio2.jpg";
 
 function Home () {
     return (
+        <>
         <HomeWrapper>
-            <HomePicture src={Picture_1} alt="Physioter" />
-            <HomeDescription>
+            <HomePicture src={picture_1} alt="Physioter" primary/>
+            <HomeDescription primary>
             <Title> Herzlich willkommen!</Title>
             <p>Wir freuen uns, dass Sie Interesse an unserer Praxis zeigen.</p>
             </HomeDescription>
-        </HomeWrapper>
+             </HomeWrapper>
+             
+             <TwoColumnSection>
+               <div> 
+              <Title>Phsiotherapie im Herzen <br />der Hamburger Westseite</Title>  
+              <p style={{marginTop: '15px'}}>
+               Unsere helle und moderne Praxis befindet sich in einem ruhigen, grünen Stadtteil im Westen 
+               Hamburgs. Die naturnahe Umgebung schafft eine angenehme Atmosphäre, die zur Entspannung und
+             Regeneration beiträgt. In dieser freundlichen Lage empfängt Sie unser engagiertes Team mit
+              viel Fachkompetenz und persönlichem Engagement – für Ihre Gesundheit und Ihr Wohlbefinden
+              </p>
+              </div>
+              
+             <HomePicture src={picture_2} alt="Physioter" />
+             </TwoColumnSection>
+             
+</>
+            
+       
 
         
     )
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,19 +1,37 @@
 import styled from "@emotion/styled";
 
+interface HomeDescriptionProps {
+  primary?: boolean;
+}
+
 export const HomeWrapper = styled.div`
   position: relative;
   width: 100%;
   height: 400px;
-  overflow: hidden;
+  overflow: visible;
+  flex-direction: column;
 `;
 
-export const HomePicture = styled.img`
-  width: 100%;
-  height: 100%;
+export const TwoColumnSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 350px;
+  gap: 20px;
+  padding: 20px;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+export const HomePicture = styled.img<HomeDescriptionProps>`
+  width: ${({primary}) => primary ?  '100%' : '450px'};
+  height: ${({primary}) => primary ?  '100%' : '350px'};
   object-fit: cover;
 `;
 
-export const HomeDescription = styled.div`
+export const HomeDescription = styled.div<HomeDescriptionProps>`
   position: absolute;
   bottom: 30px;
   left: 20px;
@@ -23,9 +41,10 @@ export const HomeDescription = styled.div`
   font-weight: bold;
   background-color: rgba(0, 0, 0, 0.5);
   padding: 10px 20px;
+  border-bottom: ${({ primary }) => (primary ? "3px solid #0a4b96ff" : "none")};
   border-radius: 8px;
 `;
 
 export const Title = styled.div`
-font-size: 2rem;
+  font-size: 2rem;
 `;
