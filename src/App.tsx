220d504f772e163f78e29dbdb82045c3200b042c
OlhaOliinyk <olaolijnik71@gmail.com>
2025-07-16 15:15:40 +0200

    fix: add basename for GitHub Pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   
   return (
     
-     <BrowserRouter>
+     <BrowserRouter basename="/praxis-website-react/">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
