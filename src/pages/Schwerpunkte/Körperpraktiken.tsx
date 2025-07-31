9bd6dff928dc061a63844c12dfc762a588c45500
OlhaOliinyk <olaolijnik71@gmail.com>
2025-07-31 12:58:29 +0200

    add backgroundpage to Körperpraktiken page

--- src/pages/Schwerpunkte/Körperpraktiken.tsx
+++ src/pages/Schwerpunkte/Körperpraktiken.tsx
@@ -1,17 +1,19 @@
 import Variants from "../../components/Variants/Variants";
+import './styles.css';
+import img from "../../assets/Körperpraktiken/Körperpraktiken.jpg";
 
-function Körperpraktiken () {
-return (
-    <div style={{
-        width: '100%', 
-        height: 'auto', 
-        overflow: 'visible', 
-        flexDirection: 'column',
-        margin: '10px 0',
-        }}>
-     <Variants />
+function Körperpraktiken() {
+  return (
+    <div className="wrapper">
+      <div className="background"
+       style={{
+          backgroundImage: `url(${img})`,
+        }}
+      >  
+      </div>
+      <Variants />
     </div>
-)
+  );
 }
 
 export default Körperpraktiken;
